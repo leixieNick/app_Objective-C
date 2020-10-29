cc6c48fc85dd7dcc6b58897414498b7dfefe46d2
xielei <lei_xie@asdc.com.cn>
2020-10-29 17:27:15 +0800

    工程初始化

---
+++ objective_app/objective_app/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  objective_app
+//
+//  Created by asdc on 2020/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objective_app/objective_app/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  objective_app
+//
+//  Created by asdc on 2020/10/29.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = UIColor.redColor;
+}
+
+
+@end
